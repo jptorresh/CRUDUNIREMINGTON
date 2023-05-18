8fe8419dbc5e1f58920c52c4e812cde7659acfb5
juan pablo torres <jptorres@coordinadora.com>
2023-05-18 14:04:15 -0500

    creacion form y table

---
+++ CRUD/index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud Uniremington</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+</head>
+
+<body>
+    <h1 class="text-center p3">Estudiantes Arquitectura de software</h1>
+
+    <div class="container-fluid row">
+        <form class="col-4 p-3">
+            <h3 class="text-center text-secondary">Registro Estudiantes</h3>
+            <div class="mb-3">
+                <label for="codigo" class="form-label">Codigo del estudiante</label>
+                <input type="text" class="form-control" name="codigo">
+            </div>
+            <div class="mb-3">
+                <label for="nombre" class="form-label">Nombre del estudiante</label>
+                <input type="text" class="form-control" name="nombre">
+            </div>
+            <div class="mb-3">
+                <label for="apellido" class="form-label">Apellido del estudiante</label>
+                <input type="text" class="form-control" name="apellido">
+            </div>
+            <div class="mb-3">
+                <label for="correo" class="form-label">Correo del estudiante</label>
+                <input type="mail" class="form-control" name="correo">
+            </div>
+            <div class="mb-3">
+                <label for="celular" class="form-label">Celular del estudiante</label>
+                <input type="text" class="form-control" name="celular">
+            </div>
+            <div class="mb-3">
+                <label for="ciudad" class="form-label">Ciudad del estudiante</label>
+                <input type="text" class="form-control" name="ciudad">
+            </div>
+
+
+            <button type="submit" class="btn btn-primary">Registrar</button>
+        </form>
+        <div class="col-8 p-4">
+            <table class="table">
+                <thead class="bg-warning">
+                    <tr>
+                        <th scope="col">Codigo</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Celular</th>
+                        <th scope="col">Ciudad</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>Otto</td>
+                        <td>@mdo</td>
+                        <td>
+                            <a href="">Editar</a>
+                            <a href="">Eliminar</a>
+
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
